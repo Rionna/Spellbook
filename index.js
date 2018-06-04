8309667b6154eb1927f9549d238571920053ab92
Rionna <Rionna@Rionnas-MacBook-Air.local>
2018-06-04 14:51:01 -0400

    Day 1 Homework

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('testing')
+const button = document.querySelector('button')
+function sayContents (){
+  const heading = document.querySelector('h1')
+  heading.textContent = 'This Spellbook Holds All Secrets'
+}
+
+button.addEventListener('click', sayContents)
+
+function changeSecond(){
+    document.getElementById('secondHeader').innerHTML = "The Benefits Will Hurt Someone Else"
+}
+
+function form(){
+    document.getElementById('submit').innerText = 'header'
+}
+
+document.getElementById('Submit').onkeydown = function(e){
+    if(e.keyCode == 13){}
+}
