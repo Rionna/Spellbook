45d785bfdf3190e9569c7c9aacb0e2b294172f83
Rionna <Rionna@Rionnas-MacBook-Air.local>
2018-06-05 22:21:31 -0400

    Updated To Day 2 Homework

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const changeHeading = function(ev) {
   const spellName = f.spellName.value
 
   const spellsDiv = document.querySelector('#spells')
-  spellsDiv.innerHTML += `<li>${spellName}</li>`
 
+  var textColor = document.getElementById('spellColor').value;
+
+  //spellsDiv.innerHTML += `<li>${spellName}</li>`
+
+  var x = document.createElement("LI");
+  var t = document.createTextNode(spellName);
+  t.se
+  x.appendChild(t);
+  x.style.color = textColor;
+  document.getElementById("spells").appendChild(x);
+  
   f.reset()
 }
 
